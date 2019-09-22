c2594131ccbf581c820c676dd4f93cff229e89b7
m.morshinin <m.morshinin@b2b-center.ru>
2019-09-22 17:54:18 +0300

    Попытка поставить метку на карте

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-import { mapInit as map } from './js/ymaps';
+import {mapInit as map} from './js/ymaps';
 
 window.onload = map();
 
 const myMap = document.querySelector('#map');
 
 myMap.addEventListener('click', () => {
-    console.log('ok');
+    const x = 55;
+    const y = 37;
+
+    let myObj = new map.GeoObject({
+        geometry: {
+            type: 'Point',
+            coordinates: [x, y]
+        },
+        properties: {
+            clusterCaption: 'So lovely',
+            balloonContentBody: 'lorem ipsum'
+        }
+    })
+
+    map.geoObjects.add(myObj);
 });
